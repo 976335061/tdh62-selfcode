5c16464b1edc514a95257893f64160b856420528
tdh62 <821028463@qq.com>
2016-06-10 14:08:36 +0800

    Update ls.c

--- ls.c
+++ ls.c
@@ -1,18 +1,17 @@
 #include"stdio.h"
-#defineN5
-intReadDat(inta[N]){
-	intx;
+#define N 1
+static int a[N],i,j,k,l,x,cnt=0,cnt1=0,sum=0,sum1=0;
+static float pjz1=0,pjz2=0;
+int ReadDat(int a[N]){
 printf("请输入%d个四位数:\n",N);
 for(x=0;x<N;x++)
 {
 printf("a[%d]:",x);
 scanf("%d",&a[x]);
 }
-return0;
+return 0;
 }
-floatjsValue(inta[N]){
-inti,j,k,l,x,cnt=0,cnt1=0,sum=0,sum1=0;
-floatpjz1,pjz2;
+float jsValue(int a[N]){
 for(x=0;x<N;x++){
 i=a[x]/1000;
 j=(a[x]%1000)/100;
@@ -25,17 +24,16 @@ else{
 cnt1=cnt1+1;
 sum1=sum1+a[x];}
 }
-pjz1=sum/cnt;
-pjz2=sum1/cnt1;
-return0;
+pjz1=(float)sum/cnt;
+pjz2=(float)sum1/cnt1;
+return 0;
 }
-floatWriteDat(floatpjz1,floatpjz2){
-	printf("满足该条件的所有数的平均值为%f\n不满足该条件的所有数的平均值为%f\n",pjz1,pjz2);
-	return0;
+float WriteDat(float pjz1,float pjz2){
+printf("满足该条件的所有数的平均值为%f\n不满足该条件的所有数的平均值为%f\n",pjz1,pjz2);
+return 0;
 }
-voidmain(){
-inta[N];
-floatpjz1,pjz2;
+void main(){
+
 ReadDat(a);
 jsValue(a);
 WriteDat(pjz1,pjz2);
